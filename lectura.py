c49643fe2a428b99fa080a61ee75f0f12440e4b3
Omar Sanseviero <osanseviero@users.noreply.github.com>
2018-09-25 15:23:27 -0500

    feat: Improve performance of maze reading

--- lectura.py
+++ lectura.py
@@ -6,33 +6,20 @@ import time
 start_time = time.time()
 
 lineno = 0
-row = 0
-
+labyrinth = []
 for line in fileinput.input():
     if lineno > 2:
-        maze = re.findall(r"[-+]?\d*\.\d+|\d+", line)
-        labyrinth[row] = [int(x) for x in str(maze[0])]
-        row += 1
-    else:
-        if lineno == 0:
-            size = re.findall(r"[-+]?\d*\.\d+|\d+", line)
-            print(size)
-            size = list(map(int, size))
-            labyrinth = [0] * size[1]
-            for i in range(size[1]):
-                labyrinth[i] = [0] * size[0]
+        labyrinth.append(list(line)[:-1])
+    elif lineno == 1:
+        start = re.findall(r"[-+]?\d*\.\d+|\d+", line)
+        start = list(map(int, start))
 
-        elif lineno == 1:
-            start = re.findall(r"[-+]?\d*\.\d+|\d+", line)
-            start = list(map(int, start))
-
-        elif lineno == 2:
-            end = re.findall(r"[-+]?\d*\.\d+|\d+", line)
-            end = list(map(int, end))
-
+    elif lineno == 2:
+        end = re.findall(r"[-+]?\d*\.\d+|\d+", line)
+        end = list(map(int, end))
     lineno += 1
 
-print(labyrinth)
+#print(labyrinth)
 
-end = time.time()
-print(end - start_time)
+end_time = time.time()
+print(end_time - start_time)
