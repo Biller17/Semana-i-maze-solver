80e5fefed6a4ddd01fd1bf467bc8d4e4c1cfe2cb
Omar Sanseviero <osanseviero@users.noreply.github.com>
2018-09-25 15:09:54 -0500

    feat: Remove O(n^2) to make algorithm faster

--- lectura.py
+++ lectura.py
@@ -1,31 +1,38 @@
 import fileinput
 import re
 
+import time
+
+start_time = time.time()
+
 lineno = 0
 row = 0
 
 for line in fileinput.input():
-    if lineno == 0:
-        size = re.findall(r"[-+]?\d*\.\d+|\d+", line)
-        size = list(map(int, size))
-        labyrinth = [0] * size[1]
-        for i in range(size[1]):
-            labyrinth[i] = [0] * size[0]
-
-    if lineno == 1:
-        start = re.findall(r"[-+]?\d*\.\d+|\d+", line)
-        start = list(map(int, start))
-
-    if lineno == 2:
-        end = re.findall(r"[-+]?\d*\.\d+|\d+", line)
-        end = list(map(int, end))
-
     if lineno > 2:
         maze = re.findall(r"[-+]?\d*\.\d+|\d+", line)
-        mazerow = [int(x) for x in str(maze[0])]
-        for i in range(size[0]):
-            labyrinth[row][i] = mazerow[i]
+        labyrinth[row] = [int(x) for x in str(maze[0])]
         row += 1
+    else:
+        if lineno == 0:
+            size = re.findall(r"[-+]?\d*\.\d+|\d+", line)
+            print(size)
+            size = list(map(int, size))
+            labyrinth = [0] * size[1]
+            for i in range(size[1]):
+                labyrinth[i] = [0] * size[0]
+
+        elif lineno == 1:
+            start = re.findall(r"[-+]?\d*\.\d+|\d+", line)
+            start = list(map(int, start))
+
+        elif lineno == 2:
+            end = re.findall(r"[-+]?\d*\.\d+|\d+", line)
+            end = list(map(int, end))
+
     lineno += 1
 
 print(labyrinth)
+
+end = time.time()
+print(end - start_time)
