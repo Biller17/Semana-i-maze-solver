a853b8a5bd45fd0407e2c93b27388ea2b1a93938
Omar Sanseviero <osanseviero@users.noreply.github.com>
2018-09-27 17:34:16 -0500

    feat: Change matrix to graph

--- mazeSolver.py
+++ mazeSolver.py
@@ -8,109 +8,86 @@ Omar Sanseviero Güezmes A01021626
 import fileinput
 import re
 import queue
+from collections import defaultdict
 
 
-#Node class for all nodes in the search tree
-class Node:
-    def __init__(self, position, maze, exit, moveDone, father):
-        self.maze = maze
-        self.position = position
-        self.exit = exit
-        self.moveDone = moveDone
-        self.father = father
-        self.childNodes = []
-        self.heuristic = 0
+exit = None
+visited = None
+maze = None
 
-    def checkHeuristic(self):
-        heuristic = 2*(abs(self.position[0] - self.exit[0]) + abs(self.position[1] - self.exit[1]))
-        self.heuristic = heuristic
-
-    def __lt__(self,other):
-        return (self.heuristic<other.heuristic)
-
-
-    #returns an array of all possible moves in current zero position
-    def checkPossibleMoves(self):
-        posX  = self.position[0]
-        posY = self.position[1]
-        labyrinthSize = len(self.maze)-1
-        moves = []
-        #check Up
-        # print("(", posX, ",", posY, ")")
-        if((labyrinthSize - posY - 1) >= 0):
-            if(self.maze[labyrinthSize-posY - 1][posX] == '0'):
-                moves.append(["U",[posX, posY+1]])
-            #up is possible
-        #check Down
-        if((labyrinthSize - posY + 1) <= labyrinthSize):
-            if(self.maze[labyrinthSize-posY + 1][posX] == '0'):
-                moves.append(["D", [posX, posY-1]])
-        #check Right
-        if(posX + 1 < len(self.maze[0])):
-            if(self.maze[labyrinthSize - posY][posX + 1] == '0'):
-                moves.append(["R",[posX+1, posY]])
-        #check Left
-        if(posX - 1 >= 0):
-            if(self.maze[labyrinthSize - posY][posX - 1] == '0'):
-                moves.append(["L",[posX-1, posY]])
-        return moves
-
-    def createChildren(self):
-        possibleMoves = self.checkPossibleMoves()
-        for i in range(len(possibleMoves)):
-            self.childNodes.append(Node(possibleMoves[i][1], self.maze, self.exit, possibleMoves[i][0], self))
-        return self.childNodes
+def checkHeuristic(child):
+    heuristic = 2*(abs(child[0] - exit[0]) + abs(child[1] - exit[1]))
+    return heuristic
 
 #a* algorithm with heuristic of out of place numbers
 def aStar():
+    path = ''
     #this will be the queue array to check the nodes
-    maze, start, exit = readLabyrinth()
-    # print(start)
-    visitedNodes = []
-    foundExit = False
-    root = Node(start, maze, exit ,"", None)
-
+    maze, start = readLabyrinth()
+    root = maze[tuple(start)]
     pq = queue.PriorityQueue()
     pq.put((0, root))
 
     numberOfActions = 0
     while(not pq.empty()):
         currentNode = pq.get()[1]
+        visited[currentNode[0][0]] = True
         #if current node does not have the answer then expand and create children with possible moves and add them to the queue
-        if(currentNode.position != exit):
-            visitedNodes.append(currentNode.position)
-            children = currentNode.createChildren()
-            for i in range(len(children)):
-                # print("child")
-                if(children[i].position not in visitedNodes):
-                    children[i].checkHeuristic()
-                    pq.put((children[i].heuristic, children[i]))
+        if(list(currentNode[0][0]) != exit):
+            for child in currentNode:
+                if(not visited[child[1]]):
+                    print(child[1])
+                    heuristic = checkHeuristic(child[1])
+                    #maze[child[1]].append(currentNode[0][0])
+                    pq.put((heuristic, maze[child[1]]))
         else:
-            foundExit = True
             solution = ''
-            while (currentNode.father != None):
-                solution += currentNode.moveDone
-                currentNode = currentNode.father
-            return solution[::-1]
+            while (currentNode != None):
+                pass
+                #print(currentNode)
+            return "".join(solution[::-1])
         numberOfActions+=1
+    print("-")
 
 
 def readLabyrinth():
+    global exit
+    global visited
     lineno = 0
-    labyrinth = []
+    graph = defaultdict(list)
+    visited = {}
+    prevRow = []
     for line in fileinput.input():
         if lineno > 2:
-            labyrinth.append(list(line)[:-1])
+            row = list(line)[:-1]
+            for i, elem in enumerate(row):
+                if elem == '0':
+                    currentLine = lineno-3
+                    visited[(i,currentLine)] = False
+                    if currentLine > 0:
+                        # Add current to previous row
+                        if prevRow[i] == '0':
+                            graph[(i, currentLine-1)].append([(i, currentLine-1), (i, currentLine), 'D'])
+                            graph[(i, currentLine)].append([(i, currentLine), (i, currentLine-1), 'U'])
+                    if i+1 < len(row):
+                        if row[i+1] == '0':
+                            graph[(i, currentLine)].append([(i, currentLine), (i+1, currentLine), 'R']) 
+                            graph[(i+1, currentLine)].append([(i+1, currentLine), (i, currentLine), 'L'])
+            prevRow = row
+        elif lineno == 0:
+            size = re.findall(r"[-+]?\d*\.\d+|\d+", line)
+            size = list(map(int, size))
         elif lineno == 1:
             start = re.findall(r"[-+]?\d*\.\d+|\d+", line)
             start = list(map(int, start))
-
+            start = [start[0], size[1]-start[1]-1]
         elif lineno == 2:
             end = re.findall(r"[-+]?\d*\.\d+|\d+", line)
-            end = list(map(int, end))
+            exit = list(map(int, end))
+            exit = [exit[0], size[1]-exit[1]-1]
         lineno += 1
-    return labyrinth, start, end
+    return graph, start
 
 
 if __name__ == '__main__':
-    print("".join(aStar()))
+    aStar()
